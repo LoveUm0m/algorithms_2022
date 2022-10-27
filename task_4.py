8d41c7f001ac2c4354e713b371dda734fce900a8
Vladyha_ <vlad.htcx97@gmail.com>
2022-10-27 17:44:07 +0300

    Lesson_5

--- task_4.py
+++
@@ -1,9 +0,0 @@
-def my_sum(n):
-    if n == 1:
-        return 1
-    else:
-        return my_sum(n-1) + (-0.5) ** (n-1)
-
-
-n = int(input('Введите количество элементов: '))
-print(f'Количество элементов - {n}, их сумма -', my_sum(n))
